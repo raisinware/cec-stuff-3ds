7a3f8a106a4652cbede9b1f95f014a8692cf8d8d
raisinware <102261580+raisinware@users.noreply.github.com>
2023-06-07 00:34:17 -0700

    refactor 2
    ipc wrappers are now gonna be in their own separate files :33

--- libcecd/cecd.c
+++ libcecd/cecd.c
@@ -1,7 +1,5 @@
 // SPDX-License-Identifier: MPL-2.0+
 #include "cecd.h"
-#include "cecd-priv.h"
-#include <3ds/ipc.h>
 #include <3ds/result.h>
 #include <3ds/srv.h>
 #include <3ds/svc.h>
@@ -37,36 +35,4 @@ void cecdExit(void) {
 
 Handle cecdGetSessionHandle(void) {
 	return cecdHandle;
-}
-
-Result CECD_RunCommand(CEC_Command command) {
-	Result res = -1;
-	u32* cmdbuf = getThreadCommandBuffer();
-
-	cmdbuf[0] = IPC_MakeHeader(CID_RUNCOMMAND, 1, 0); // 0x000B0040
-	cmdbuf[1] = command;
-
-	res = svcSendSyncRequest(cecdHandle);
-
-	if (R_FAILED(res)) {
-		return res;
-	}
-
-	return (Result)cmdbuf[1];
-}
-
-Result CECD_RunCommandAlt(CEC_Command command) {
-	Result ret = -1;
-	u32* cmdbuf = getThreadCommandBuffer();
-
-	cmdbuf[0] = IPC_MakeHeader(CID_RUNCOMMANDALT, 1, 0); // 0x000C0040
-	cmdbuf[1] = command;
-
-	ret = svcSendSyncRequest(cecdHandle);
-
-	if (R_FAILED(ret)) {
-		return ret;
-	}
-
-	return (Result)cmdbuf[1];
 }
---
+++ libcecd/run_command.c
@@ -0,0 +1,38 @@
+#include "cecd-priv.h"
+#include "cecd.h"
+#include <3ds/ipc.h>
+#include <3ds/result.h>
+#include <3ds/synchronization.h>
+#include <3ds/types.h>
+
+Result CECD_RunCommand(CEC_Command command) {
+	Result res = -1;
+	u32* cmdbuf = getThreadCommandBuffer();
+
+	cmdbuf[0] = IPC_MakeHeader(CID_RUNCOMMAND, 1, 0); // 0x000B0040
+	cmdbuf[1] = command;
+
+	res = svcSendSyncRequest(cecdGetSessionHandle());
+
+	if (R_FAILED(res)) {
+		return res;
+	}
+
+	return (Result)cmdbuf[1];
+}
+
+Result CECD_RunCommandAlt(CEC_Command command) {
+	Result ret = -1;
+	u32* cmdbuf = getThreadCommandBuffer();
+
+	cmdbuf[0] = IPC_MakeHeader(CID_RUNCOMMANDALT, 1, 0); // 0x000C0040
+	cmdbuf[1] = command;
+
+	ret = svcSendSyncRequest(cecdGetSessionHandle());
+
+	if (R_FAILED(ret)) {
+		return ret;
+	}
+
+	return (Result)cmdbuf[1];
+}
