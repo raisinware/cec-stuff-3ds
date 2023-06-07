80b4470310a962fbc155d5547f64be0468b81e32
raisinware <102261580+raisinware@users.noreply.github.com>
2023-06-07 11:22:38 -0700

    implement CECD_GetCecStateAbbreviated
    adds a new set of enums, CECD_GetCecStateAbbreviated(), and also adds some more comments to CEC_Command :3

--- libcecd/cecd.h
+++ libcecd/cecd.h
@@ -3,9 +3,12 @@
 #include <3ds/types.h>
 
 /// Command to send to CECD
+/// TODO: more detailed descriptions for each command
 typedef enum {
+	/// Does nothing
 	CEC_COMMAND_NONE                  = 0x00,
 
+	/// 
 	CEC_COMMAND_START                 = 0x01,
 	CEC_COMMAND_RESET_START           = 0x02,
 	CEC_COMMAND_READYSCAN             = 0x03,
@@ -28,13 +31,30 @@ typedef enum {
 	CEC_COMMAND_DAEMON_START          = 0x10,
 	CEC_COMMAND_EXIT                  = 0x11,
 
-	// streetpass relay (over BOSS) commands
+	/// streetpass relay (over BOSS) commands
 	CEC_COMMAND_OVER_BOSS             = 0x12,
 	CEC_COMMAND_OVER_BOSS_FORCE       = 0x13,
 	CEC_COMMAND_OVER_BOSS_FORCE_WAIT  = 0x14,
 
+	/// End command.
 	CEC_COMMAND_END                   = 0x15,
 } CEC_Command;
+
+/// Abbreviated state of CECD.
+typedef enum {
+	/// Corresponds to CEC_STATE_IDLE 
+	CEC_STATE_ABBREV_IDLE     = 1,
+	/// Corresponds to CEC_STATEs *FINISH*, *POST, and OVER_BOSS 
+	CEC_STATE_ABBREV_INACTIVE = 2,
+	/// Corresponds to CEC_STATE_SCANNING 
+	CEC_STATE_ABBREV_SCANNING = 3,
+	/// Corresponds to CEC_STATE_WIRELESS_READY when some unknown bool is
+	/// true 
+	CEC_STATE_ABBREV_WLREADY  = 4,
+	/// Corresponds to CEC_STATEs besides *FINISH*, *POST, and OVER_BOSS and
+	/// those listed here 
+	CEC_STATE_ABBREV_OTHER    = 5,
+} CEC_StateAbbreviated;
 
 /// Initializes communication with CECD (streetpass daemon)
 Result cecdInit(void);
@@ -50,3 +70,6 @@ Result CECD_RunCommand(CEC_Command command);
 
 /// Start a command on CECD, but with some extra checks.
 Result CECD_RunCommandAlt(CEC_Command command);
+
+/// Output the abbreviated CECD state to *out*
+Result CECD_GetCecStateAbbreviated(CEC_StateAbbreviated* out);
---
+++ libcecd/get_cec_state_abbreviated.c
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: MPL-2.0+
+#include "cecd-priv.h"
+#include "cecd.h"
+#include <3ds/ipc.h>
+
+Result CECD_GetCecStateAbbreviated(CEC_StateAbbreviated* out) {
+	Result res = 0;
+	u32* cmdbuf = getThreadCommandBuffer();
+
+	cmdbuf[0] = IPC_MakeHeader(CID_GETCECSTATEABBR, 0, 0); // 0x000E0000
+
+	res = svcSendSyncRequest(cecdGetSessionHandle());
+
+	if (R_FAILED(res)) {
+		return res;
+	}
+
+	res = (Result)cmdbuf[1];
+
+	if (R_SUCCEEDED(res) && out) {
+		*out = cmdbuf[2];
+	}
+
+	return res;
+}
